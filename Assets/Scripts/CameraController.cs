141bbd4f73edea000ae83def2e81758b756e90b3
Krittin Meenrattanakorn <whatevermarch@gmail.com>
2017-05-04 23:35:27 +0700

    Add Multiplayer Feature

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	private Transform playerTransform;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if(playerTransform != null)
+		{
+			transform.position = playerTransform.position + new Vector3 (0.0f, 9.0f, -5.0f);
+		}
+	}
+
+	public void setTarget(Transform target)
+	{
+		playerTransform = target;
+	}
+
+}
---
+++ Assets/Scripts/NetworkManagerForTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(NetworkManager))]
+public class NetworkManagerForTest : MonoBehaviour {
+
+	private NetworkManager manager;
+
+	// Use this for initialization
+	void Start () {
+		manager = GetComponent<NetworkManager>();
+		manager.StartHost ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public static void OnConnected(NetworkMessage netMsg)
+	{
+		Debug.Log("Host activated");
+	}
+
+	void OnDestroy(){
+		manager.StopHost ();
+	}
+}
